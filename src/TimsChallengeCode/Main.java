e3f611b5d68e70991bcc710c88fb22b193a8eb5d
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-18 13:56:51 +0200

    Initializing the bedroom class

--- src/TimsChallengeCode/Main.java
+++ src/TimsChallengeCode/Main.java
@@ -11,6 +11,15 @@ public class Main {
 	then add at least one method to hide the object i.e. not using a getter
 	but to access the object used in composition within the main class
 	 */
+        //Variables needed for initializing the bedroom
+        Wall wall1 = new Wall("Front");
+        Wall wall2 = new Wall("Back");
+        Wall wall3 = new Wall("Left");
+        Wall wall4 = new Wall("Right");
+        Ceiling ceiling = new Ceiling(3, 1);
+        Bed bed = new Bed("Classic", 3, 1, 1, 1);
+        Lamp lamp = new Lamp("Modern", false, 5);
+        //finally creating the bedroom
         Bedroom bedroom = new Bedroom("Tim's bedroom", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
     }
 }
